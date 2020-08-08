0f3ea5463757487ff1727b05fa0e2e9379798edd
noejasiel <55604384+noejasiel@users.noreply.github.com>
2020-08-07 22:38:38 -0500

    conforme bajes se se mostrara una animacion

--- main.js
+++ main.js
@@ -18,14 +18,45 @@ btn.addEventListener("click", function hola() {
     padre_nav.classList.remove("navbar-activo");
   }
 });
+// mover mis span a medida que el scroll baja
+var span1 = [];
+var span2 = [];
+var U_card = document.getElementsByClassName("U-card");
+for (const iterator of U_card) {
+  span1.push(iterator.querySelector("span"));
+  span2.push(iterator.querySelector("span").nextElementSibling);
+}
+
+// console.log(span1);
+// console.log(span2);
+
+// for (let i = 0; i < spans.length; i++) {
+//   console.log(spans[i]);
+// }
 
 window.onscroll = function () {
-  if (window.pageYOffset > 200) {
-    btn.parentElement.parentElement.classList.add("navbar-activo");
-    console.log("estoy a mas de 200");
+  // if (window.pageYOffset > 300) {
+  //   btn.parentElement.parentElement.classList.add("navbar-activo");
+  //   console.log("estoy a mas de 300");
+  // } else {
+  //   btn.parentElement.parentElement.classList.remove("navbar-activo");
+  // }
+
+  // conforme se acerque a mi apartade de tecnologia agregaraemos la clase a mis span
+  if (window.pageYOffset >= 1000 && window.pageYOffset <= 1300) {
+    for (const iterator of span1) {
+      iterator.classList.add("span-arrow1-active");
+    }
+    for (const iterator of span2) {
+      iterator.classList.add("span-arrow2-active");
+    }
   } else {
-    console.log("estoy a menos de 200");
-    btn.parentElement.parentElement.classList.remove("navbar-activo");
+    for (const iterator of span1) {
+      iterator.classList.remove("span-arrow1-active");
+    }
+    for (const iterator of span2) {
+      iterator.classList.remove("span-arrow2-active");
+    }
   }
 };
 
@@ -35,6 +66,7 @@ var btn_inicio = document.getElementById("bnt-inicio");
 var btn_formacion = document.getElementById("bnt-formacion");
 var btn_proyectos = document.getElementById("bnt-proyectos");
 var btn_contacto = document.getElementById("bnt-contacto");
+var btn_tecnologia = document.getElementById("bnt-tecnologia");
 
 btn_inicio.addEventListener("click", function () {
   btn.classList.remove("btn-mobile-activo");
@@ -44,6 +76,10 @@ btn_inicio.addEventListener("click", function () {
   }
 });
 btn_formacion.addEventListener("click", function () {
+  btn.classList.remove("btn-mobile-activo");
+  btn.nextElementSibling.classList.remove("menu-mobile");
+});
+btn_tecnologia.addEventListener("click", function () {
   btn.classList.remove("btn-mobile-activo");
   btn.nextElementSibling.classList.remove("menu-mobile");
 });
